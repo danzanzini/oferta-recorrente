d5c90362168ab47fefaa9d5ff0ec4ba2d2bdae1d
Daniel Zanzini <danzan@Daniels-MacBook-Air.local>
2024-04-16 09:59:20 -0300

    Add authentication routes

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,26 @@
+# test/controllers/sessions_controller_test.rb
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test "should log in with valid credentials" do
+    post session_url, params: { email: @user.email, password: 'secret' }
+    assert_redirected_to root_url
+    assert_equal @user.id, session[:user_id]
+  end
+
+  test "should not log in with invalid credentials" do
+    post session_url, params: { email: @user.email, password: 'wrong' }
+    assert 'new'
+    assert_nil session[:user_id]
+  end
+
+  test "should log out" do
+    delete session_url(@user)
+    assert_redirected_to root_url
+    assert_nil session[:user_id]
+  end
+end
