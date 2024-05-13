b9102d6ce08c0e988f03399b921dc4e3f3d937d6
Daniel Zanzini <dpzanzini@gmail.com>
2024-05-13 13:30:31 -0300

    Adds method to check if offering is open or closed

--- app/models/offering.rb
+++ app/models/offering.rb
@@ -2,4 +2,8 @@ class Offering < ApplicationRecord
   belongs_to :location
   belongs_to :organization
   acts_as_tenant :organization
+
+  def is_open?(now)
+    opens_at <= now && closes_at >= now
+  end
 end
--- test/models/offering_test.rb
+++ test/models/offering_test.rb
@@ -1,7 +1,22 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class OfferingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @offering = offerings(:one)
+  end
+
+  test 'its open at a given time' do
+    now = Time.now
+    @offering.opens_at = now - 1.hour
+    @offering.closes_at = now + 1.hour
+    assert @offering.is_open?(now)
+  end
+
+  test 'its closed at a given time' do
+    now = Time.now
+    @offering.opens_at = now + 1.hour
+    assert !@offering.is_open?(now)
+  end
 end
