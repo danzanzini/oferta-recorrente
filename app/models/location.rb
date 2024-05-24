a7456de95139df179010a03da766c1646750cbb3
Daniel Zanzini <dpzanzini@gmail.com>
2024-05-24 13:43:46 -0300

    Adds validation to Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,8 @@
 class Location < ApplicationRecord
   acts_as_tenant :organization
-
   has_many :offerings
 
+  validates :name, :pickup_place, :address, presence: true
   def current_offering
     offerings.open_now.last
   end
--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class LocationsControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -7,40 +9,63 @@ class LocationsControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
   end
 
-  test "should get index" do
+  def valid_location_params
+    { location: { address: @location.address, link: @location.link, name: @location.name,
+                  organization_id: @location.organization_id, pickup_place: @location.pickup_place } }
+  end
+
+  def invalid_location_params
+    { location: { name: nil } }
+  end
+
+  test 'should get index' do
     get locations_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_location_url
     assert_response :success
   end
 
-  test "should create location" do
-    assert_difference("Location.count") do
-      post locations_url, params: { location: { address: @location.address, link: @location.link, name: @location.name, organization_id: @location.organization_id } }
+  test 'should create location' do
+    assert_difference('Location.count') do
+      post locations_url, params: valid_location_params
     end
 
     assert_redirected_to location_url(Location.last)
   end
 
-  test "should show location" do
+  test 'should not create location when invalid' do
+    assert_no_difference('Location.count') do
+      post locations_url, params: invalid_location_params
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'should show location' do
     get location_url(@location)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_location_url(@location)
     assert_response :success
   end
 
-  test "should update location" do
-    patch location_url(@location), params: { location: { address: @location.address, link: @location.link, name: @location.name, organization_id: @location.organization_id } }
+  test 'should update location' do
+    patch location_url(@location), params: valid_location_params
     assert_redirected_to location_url(@location)
   end
 
-  test "should deactivate location" do
+  test 'should not update location' do
+    patch location_url(@location), params: invalid_location_params
+    assert_response :unprocessable_entity
+  end
+
+
+  test 'should deactivate location' do
     delete location_url(@location)
     assert !@location.reload.active
     assert_redirected_to locations_url
